379a148b7e67e16ab22d13f6f3f81a6cb938148e
juanengml <juanengml@gmail.com>
2018-08-20 20:34:45 -0400

    Update

---
+++ aula2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// NÃO CRIAR VARIABEL GLOBAL PQ DANIEL N GOSTA RSRSR
+
+int f(int x){
+   return (x * x + 2*x +3 );
+}
+
+
+int main(){
+  printf("f(2) = %d \n",f(2));
+}
+//	printf("%d\n",*(int*)(endereço de memoria);\
+}
+
+//de referencia de um inteiro para poteiro para um endereço
+
