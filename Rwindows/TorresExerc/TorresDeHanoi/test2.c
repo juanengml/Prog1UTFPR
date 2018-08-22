1b6df2350b1b78de4e1743fbabc55217cf4edd4e
juanmanoel <37448831+juanengml@users.noreply.github.com>
2018-08-22 14:12:42 -0400

    Update test2.c

--- Rwindows/TorresExerc/TorresDeHanoi/test2.c
+++ Rwindows/TorresExerc/TorresDeHanoi/test2.c
@@ -9,12 +9,20 @@ int main()
     Torre t1 = criaTorreCheia();
     Torre t2 = criaTorreVazia();
     Torre t3 = criaTorreVazia();
+  
+    system("cls");
+    imprimeTorre(&t1);
+    imprimeTorre(&t2);
+    imprimeTorre(&t3);
+    getchar();
+    moveDisco(&t1, &t2);
 
-   statusTorres();
-   moveDisco(&t1, &t2);
-
-   statusTorres();
-   moveDisco(&t1, &t3);
+    system("cls");
+    imprimeTorre(&t1);
+    imprimeTorre(&t2);
+    imprimeTorre(&t3);
+    getchar();
+    moveDisco(&t1, &t3);
 
 
 
