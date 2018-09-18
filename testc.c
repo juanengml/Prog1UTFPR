56d86fecfe39caa3d4f8add97f381e58387947df
juanengml <juanengml@gmail.com>
2018-09-18 14:53:50 -0300

    Update new datas

---
+++ testc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+	int a;
+	char ue[] = "test1";
+
+	a = strlen(ue);
+	printf("conoteudo %d\n", a);
+	
+	return 0;
+
+}
