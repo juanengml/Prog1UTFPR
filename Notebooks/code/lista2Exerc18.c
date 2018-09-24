f336ef1a3b6b8fbd0cb6c1b51d4769c5c950686f
juanmanoel <37448831+juanengml@users.noreply.github.com>
2018-09-24 15:02:45 -0300

    Create lista2Exerc18.c

---
+++ Notebooks/code/lista2Exerc18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/***
+18) Escreva um programa que receba como
+entrada um inteiro e retorne seu quadrado
+***/
+
+int main(){
+ int inteiro, quadrado;
+
+ printf("Valor: ");
+ scanf("%d",&inteiro);
+
+ quadrado = inteiro * inteiro ;
+ printf("O Quadrado de %d = %d",inteiro, quadrado);
+
+
+}
+
