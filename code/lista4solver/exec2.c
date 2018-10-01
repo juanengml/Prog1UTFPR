875a273ccfd8a7347c2bdf052a7ccc8872a7a016
juanengml <juanengml@gmail.com>
2018-10-01 00:11:23 -0300

    Update

--- code/lista4solver/exec2.c
+++ code/lista4solver/exec2.c
@@ -14,3 +14,28 @@ receberá
 2 nota(s) de R$ 10,00
 2 nota(s) de R$ 1,00
 ***/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void saque(int entrada){
+	int notas50,notas10,notas1,resto50,resto10;
+	int saida[] = {0,1,2};
+    notas50 = entrada / 50 ;
+    resto50 = entrada % 50 ;
+    notas10 = resto50 / 10 ;
+    resto10 = notas50 % 10 ;
+    notas1  = resto50 % 10 ;
+    printf("Notas de R$ 50,00 = %d\n",notas50);
+    printf("Notas de R$ 10,00 = %d\n",notas10);
+    printf("Notas de R$ 1,00 = %d\n",notas1);
+    
+}
+
+int main(){
+	int entrada;
+	printf("Digite Valor: ");
+	scanf("%d",&entrada);
+	saque(entrada);
+	return 0;
+}
--- code/lista4solver/exec2.py
+++ code/lista4solver/exec2.py
@@ -1,5 +1,14 @@
 
-notas = [1,10,50]
+def saque(entrada):
+  notas50 = entrada / 50 # 6
+  resto50 = entrada % 50 # 33
+  notas10 = resto50 / 10 # 33 / 10 = 3
+  resto10 = notas50 % 10 # 3
+  notas1  = resto50 % 10
+  return [notas50,notas10,notas1]
 
-entrada = int(raw_input("Qual o valor do saque ? "))
-print entrada
+
+while True:
+ entrada = int(raw_input("Qual o valor do saque ? "))
+ print saque(entrada)
+ 
