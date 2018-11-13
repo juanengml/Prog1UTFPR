1cfb15a46ba0b44dc617e6ff6aab8ed525bddd8b
juanengml <juanengml@gmail.com>
2018-11-13 17:48:00 -0200

    Update animais

---
+++ code/lista4solver/exec10.py
@@ -0,0 +1,18 @@
+
+"""
+10)
+Construa um programa que seja capaz de concluir qual dentre os animais seguintes 
+foi escolhido através de perguntas e respostas. Animais possíveis: leão, cavalo, homem, 
+macaco, baleia, avestruz, pingüim, pato, águia, tartarug
+a, crocodilo e cobra
+
+Exemplo:
+É mamífero: Sim
+É quadrúpede: Sim
+É carnívoro: Não
+É herbívoro: Sim
+Então o animal escolhido foi o cavalo
+"""
+m = open('animais.csv').read()
+
+print(m)
--- code/lista4solver/exec9.c
+++ code/lista4solver/exec9.c
@@ -5,4 +5,17 @@
 “i++ + ++i” são de comportamento indefinido?
 
 ***/
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+ int a;
+ int i;
+ 
+ a = a++;
+
+ printf("a: %d\n", ++a);
+ printf("i: %d\n",i++);
+ printf("i: %d\n",++i);
+ return 0;
+}
